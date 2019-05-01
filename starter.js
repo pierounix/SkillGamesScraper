07aca071d56890f6894562f102bc22b9de202a00
pierounix <pierounix@gmail.com>
2019-05-01 20:54:23 +0200

    UserAgent

--- starter.js
+++ starter.js
@@ -19,6 +19,7 @@ console.log(lottomaticaURL);
     else
       browser = await puppeteer.launch({headless: headless, executablePath: executablePath})
     const page = await browser.newPage()
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36')
     await page.goto(lottomaticaURL)
     await Promise.all([
         page.click('#login-button'),
